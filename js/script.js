7dc88d3c5610f03135a78e4c5ec109910fd7742d
Kauã <sousakaua7@gmail.com>
2024-01-20 23:31:31 -0300

    Save

---
+++ js/script.js
@@ -0,0 +1,4 @@
+const hamburguer = document.querySelector(".hamburguer");
+const nav = document.querySelector("#myNavMenu");
+
+hamburguer.addEventListener("click", () => nav.classList.toggle("active"));
