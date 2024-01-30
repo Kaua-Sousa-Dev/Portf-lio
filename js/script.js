731536e3866e0559e606b423eb7c42fc307bf0ad
Kauã <sousakaua7@gmail.com>
2024-01-30 10:56:24 -0300

    In Process

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
+// Navbar animation
 const hamburguer = document.querySelector(".hamburguer");
 const nav = document.querySelector("#myNavMenu");
 
 hamburguer.addEventListener("click", () => nav.classList.toggle("active"));
+
+// etc
